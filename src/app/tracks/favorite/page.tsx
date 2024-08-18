94dc0126aec4816a10b5915b4a5cf06ee8740d99
Invior <twhlost@gmail.com>
2024-08-18 19:48:40 +0400

    Сделал проверку на наличие авторизации у пользователя при входе в "Мой плейлист"

--- src/app/tracks/favorite/page.tsx
+++ src/app/tracks/favorite/page.tsx
@@ -1,7 +1,14 @@
+"use client";
 import { FavoriteTracks } from "@/components/FavoriteTracks/FavoriteTracks";
+import { useAppSelector } from "@/store/store";
+import { useRouter } from "next/navigation";
 
 export default function FavoritePage() {
-  return (
-    <FavoriteTracks />
-  );
+  const user = useAppSelector((state) => state.user.user);
+  const router = useRouter();
+    if (user?.email) {
+      return <FavoriteTracks />
+    } else {
+      return router.push("/tracks");
+    }
 }
